254131d6853858c1be2d708b5069c71cf47ceb1d
Rafael Pereira <rafael.hardrock@gmail.com>
2017-04-14 13:59:49 -0300

    Realizado até Aula 5/50

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader> 
+        <h2>Nossa História</h2>
+        <p>
+            O que temos que ter sempre em mente é que a expansão dos mercados mundiais cumpre um papel essencial na formulação de alternativas às soluções ortodoxas.
+        </p>
+        <h2>Missão e Visão</h2>
+        <p>
+            O empenho em analisar o acompanhamento das preferências de consumo afeta positivamente a correta previsão dos conhecimentos estratégicos para atingir a excelência.
+        </p>
+        <h2>Imprensa</h2>
+        <p>
+            Todavia, o fenômeno da Internet deve passar por modificações independentemente das diversas correntes de pensamento.
+        </p>
+
+    </div>
+);
--- todo-app/frontend/webpack.config.js
+++ todo-app/frontend/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    "entry": "./ex/index.jsx",
+    "entry": "./src/index.jsx",
     "output": {
         "path": __dirname + "/public",
         "filename": "./bundle.js"
@@ -16,7 +16,10 @@ module.exports = {
         new ExtractTextPlugin('bundle.css')
     ],
     resolve: {
-        extensions: ['','.css','.js','.jsx']
+        extensions: ['','.css','.js','.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
     },
      module: {
         loaders: [{
@@ -32,8 +35,11 @@ module.exports = {
         {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-        }
-        ]
+        },
+        {
+            test: /\.woff|.woff2|.ttf|.eot|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
     }
 
 }
