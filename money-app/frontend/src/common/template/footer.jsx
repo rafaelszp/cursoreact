f09addfef23a1a6f4529d207bdd18de698b15706
Rafael Pereira <rafael.hardrock@gmail.com>
2017-04-18 20:47:50 -0300

    Finalizada a aula 107

---
+++ money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017&nbsp;
+            <a href='https://github.com/rafaelszp' target='_blank'>
+                Rafael Pereira
+            </a>
+        </strong>
+    </footer>
+)
